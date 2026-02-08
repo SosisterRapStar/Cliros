4bde18f4757fa18551512154a77aae37325b1d06
SosisterRapStar <timoshenkovvanya@gmail.com>
2026-02-08 15:49:31 +0300

    cur state

--- domain/outbox/reader.go
+++ domain/outbox/reader.go
@@ -2,11 +2,24 @@ package outbox
 
 import (
 	"context"
+	"sync/atomic"
 	"time"
 
 	"github.com/SosisterRapStar/LETI-paper/domain/broker"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+type OutboxMessage struct {
+	SagaID      uuid.UUID
+	StepName    string
+	Topic       string
+	CreatedAt   time.Time
+	ScheduledAt time.Time
+	Metadata    []byte
+	Payload     []byte
+	ProcessedAt time.Time
+}
 
 const defaultInterval = 1 * time.Second
 
@@ -19,6 +32,18 @@ type Reader struct {
 	Publisher       broker.Pubsub
 	PollingSettings PollingSettings
 	pool            *pgxpool.Pool
+
+	started int32
+	closed  int32
+
+	closeCtx context.Context
+	cancel   context.CancelFunc
+	errCh    chan<- error
+}
+
+func (r *Reader) IsStarted() int32 {
+	// нужно подумать над mutex здесь
+	return r.started
 }
 
 func NewPollingSettings(interval time.Duration, batchSize int) PollingSettings {
@@ -29,12 +54,19 @@ func NewPollingSettings(interval time.Duration, batchSize int) PollingSettings {
 }
 
 func (r *Reader) Start(ctx context.Context) {
+	if !atomic.CompareAndSwapInt32(&r.started, 0, 1) {
+		return
+	}
+
 	ticker := time.NewTicker(r.PollingSettings.interval)
 	defer ticker.Stop()
 	for {
 		select {
 		case <-ticker.C:
-			r.scanTable(ctx)
+			err := r.scanTable(ctx)
+			if err != nil {
+
+			}
 		}
 	}
 }
@@ -43,7 +75,6 @@ func (r *Reader) Close() {
 
 }
 
-func (r *Reader) scanBatch(ctx context.Context) {
-	conn, err := r.pool.Acquire(ctx)
-	conn.
+func (r *Reader) scanBatch(ctx context.Context) error {
+	tx, err := p.db.Begin
 }
